a7703f5ce51d5da4b7615e4d3f4821898d23098d
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-03-09 21:07:10 +0100

    Add button component styles

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  height: 55px;
+  width: 266px;
+  background-color: red;
+  border-radius: 50px;
+  color: white;
+  font-weight: bolder;
+  font-size: 22px;
+`;
+
+export default ButtonStyled;
