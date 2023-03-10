6c080e136bdef2ee0ff241df1c7a0fde1f41f334
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-03-10 13:59:06 +0100

    Add minor fixes to loginPage styles

--- src/pages/LoginPageStyled.tsx
+++ src/pages/LoginPageStyled.tsx
@@ -4,12 +4,29 @@ const LoginPageStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   gap: 80px;
+
+  h1 {
+    color: white;
+  }
+
   img {
     margin-top: 50px;
     height: 158px;
     width: 184px;
+  }
+
+  .image-container {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: url("media/photos/login.jpeg");
+    background-size: cover;
+    background-position: 65% center;
+    opacity: 0.08;
+    z-index: -999;
   }
 `;
 
